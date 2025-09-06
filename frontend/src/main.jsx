dc647fb25315efcd257dff36a2f72ae8be2db81c
Piyush Mishra <pm9958478949@gmail.com>
2025-09-06 22:29:24 +0530

    react-helmet fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
+import { HelmetProvider } from " @vuer-ai/react-helmet-async";
 import "./index.css";
 import App from "./App.jsx";
 
